e4bf6301a1f372c584c627b5684e940a59bc1302
Vladimir <vladimir.herus@centralinnovation.com>
2018-10-17 02:18:08 +1100

    Initial layout

---
+++ ExceptionSnapshotExtension/SnapshotWindowControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace ExceptionSnapshotExtension
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for SnapshotWindowControl.
+    /// </summary>
+    public partial class SnapshotWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SnapshotWindowControl"/> class.
+        /// </summary>
+        public SnapshotWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "SnapshotWindow");
+        }
+    }
+}
