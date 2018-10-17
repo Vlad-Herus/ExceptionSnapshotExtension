701eb1c796f76671a1d6787bbd44a0c2814c1a38
VladimirUAZ <VladimirGerusUA@gmail.com>
2018-10-18 01:41:49 +1100

    Looking for ways to capture exception tree

--- ExceptionSnapshotExtension/SnapshotWindowControl.xaml.cs
+++ ExceptionSnapshotExtension/SnapshotWindowControl.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace ExceptionSnapshotExtension
-{
-    using System.Diagnostics.CodeAnalysis;
-    using System.Windows;
-    using System.Windows.Controls;
+﻿using EnvDTE;
+using EnvDTE100;
+using EnvDTE80;
+using EnvDTE90;
+using Microsoft.VisualStudio.Shell;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using System.Windows.Controls;
 
+namespace ExceptionSnapshotExtension
+{
     /// <summary>
     /// Interaction logic for SnapshotWindowControl.
     /// </summary>
@@ -17,18 +22,23 @@
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
         [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void Capture(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "SnapshotWindow");
+            var dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
+
+            var debugger = dte.Debugger as Debugger3;
+            var item = debugger.ExceptionGroups.Item("Common Language Runtime Exceptions");
+
+
+            int n = 22;
+        }
+
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void Restore(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
