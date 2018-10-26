0997a28d0cdbd7b9d6e91c12eeef473e6812d4ea
VladimirUAZ <VladimirGerusUA@gmail.com>
2018-10-27 03:21:15 +1100

    Added : internal debugger interfaces

---
+++ ExceptionSnapshotExtension/Services/ExceptionManager.cs
@@ -0,0 +1,72 @@
+﻿using EnvDTE90;
+using Microsoft.VisualStudio.Debugger.Interop;
+using Microsoft.VisualStudio.Debugger.Interop.Internal;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionSnapshotExtension.Services
+{
+    internal class ExceptionManager
+    {
+        private readonly Debugger3 m_Debugger;
+        private readonly SVsShellDebugger m_ShellDebugger;
+        private IDebuggerInternal11 m_Internal;
+
+        public ExceptionManager(Debugger3 debugger, SVsShellDebugger shellDebugger)
+        {
+            m_Debugger = debugger;
+            m_ShellDebugger = shellDebugger;
+
+            m_Internal = (IDebuggerInternal11)shellDebugger;
+        }
+
+        public void EnableAll()
+        {
+            EXCEPTION_INFO[] array = null;
+            uint num = 0u;
+            IEnumDebugExceptionInfo2 val = default(IEnumDebugExceptionInfo2);
+            EXCEPTION_INFO[] array2;
+            if (m_Internal.CurrentSession.EnumDefaultExceptions(array, out val) == 0 && val != null)
+            {
+                uint num2 = default(uint);
+                val.GetCount(out num2);
+                array2 = (EXCEPTION_INFO[])new EXCEPTION_INFO[num2];
+                val.Next(num2, array2, ref num);
+            }
+
+
+
+            SetAll(true);
+        }
+
+        public void DisableAll()
+        {
+            var guid = Guid.Empty;
+            m_Internal.CurrentSession.RemoveAllSetExceptions(ref guid);
+        }
+
+        private void SetAll(bool value)
+        {
+            Observable.Start(() =>
+            {
+                if (m_Debugger.ExceptionGroups != null)
+                {
+                    ExceptionSettings group = m_Debugger.ExceptionGroups.Item("Common Language Runtime Exceptions");
+
+                    foreach (ExceptionSetting exception in group)
+                    {
+                        if (exception.BreakWhenThrown != value)
+                        {
+                            group.SetBreakWhenThrown(value, exception);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
--- ExceptionSnapshotExtension/SnapshotWindowControl.xaml.cs
+++ ExceptionSnapshotExtension/SnapshotWindowControl.xaml.cs
@@ -2,7 +2,9 @@
 using EnvDTE100;
 using EnvDTE80;
 using EnvDTE90;
+using ExceptionSnapshotExtension.Services;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,31 +16,33 @@ namespace ExceptionSnapshotExtension
     /// </summary>
     public partial class SnapshotWindowControl : UserControl
     {
+        ExceptionManager m_Manager = null;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="SnapshotWindowControl"/> class.
         /// </summary>
         public SnapshotWindowControl()
         {
             this.InitializeComponent();
+            Dispatcher.VerifyAccess();
+            var debugger = (Package.GetGlobalService(typeof(DTE)) as DTE).Debugger as Debugger3;
+            var shellDebugger = Package.GetGlobalService(typeof(SVsShellDebugger)) as SVsShellDebugger;
+            m_Manager = new ExceptionManager(debugger, shellDebugger);
         }
 
         [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void Capture(object sender, RoutedEventArgs e)
+        private void EnableAll(object sender, RoutedEventArgs e)
         {
-            var dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
-
-            var debugger = dte.Debugger as Debugger3;
-            var item = debugger.ExceptionGroups.Item("Common Language Runtime Exceptions");
-
-
-            int n = 22;
+            m_Manager.EnableAll();
         }
 
         [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void Restore(object sender, RoutedEventArgs e)
+        private void DisableAll(object sender, RoutedEventArgs e)
         {
+            m_Manager.DisableAll();
+
         }
     }
 }
